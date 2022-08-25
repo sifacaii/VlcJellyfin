4219760f081a54b6e2b5fa01e0a6e098eab048a1
sifacaii <sifacai@outlook.com>
2022-08-25 14:30:04 +0800

    添加搜索

--- app/src/main/java/org/sifacai/vlcjellyfin/SearchActivity.java
+++ app/src/main/java/org/sifacai/vlcjellyfin/SearchActivity.java
@@ -9,6 +9,9 @@ import com.owen.tvrecyclerview.widget.V7GridLayoutManager;
 
 public class SearchActivity extends BaseActivity{
     private TvRecyclerView mGridContiner = null;
+    private final int limit = 24;
+    private String BaseUrl;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,10 +26,19 @@ public class SearchActivity extends BaseActivity{
         V7GridLayoutManager v7GridLayoutManager = new V7GridLayoutManager(this,6);
         mGridContiner.setLayoutManager(v7GridLayoutManager);
         mGridContiner.setItemAnimator(null);  //防崩溃
-        init();
+
+        BaseUrl = "/Users/"+Utils.UserId+"/Items?";
+        BaseUrl += "Fields=PrimaryImageAspectRatio,CanDelete,BasicSyncInfo,MediaSourceCount";
+        BaseUrl += "&Recursive=true&EnableTotalRecordCount=false&ImageTypeLimit=1&IncludePeople=false";
+        BaseUrl += "&IncludeMedia=true&IncludeGenres=false&IncludeStudios=false&IncludeArtists=false";
+        BaseUrl += "&Limit="+limit;
     }
 
-    private void init() {
-        
+    private void Search(String searchTerm) {
+        String url = BaseUrl + "&searchTerm="+searchTerm+"&IncludeItemTypes=Movie";
+
+
+
+
     }
 }
