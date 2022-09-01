e9ce199952292db935af097bd56641487a3cc630
sifacaii <sifacai@outlook.com>
2022-09-01 11:38:19 +0800

    添加jellyfin client

--- app/src/main/java/org/sifacai/vlcjellyfin/DetailActivity.java
+++ app/src/main/java/org/sifacai/vlcjellyfin/DetailActivity.java
@@ -152,8 +152,8 @@ public class DetailActivity extends BaseActivity implements JAdapter.OnItemClick
         } else if (type.equals("Person")){
             JsonElement ProductionLocations = JfClient.jeFromGson(detailObj,"ProductionLocations");
             String PremiereDate = JfClient.strFromGson(detailObj,"PremiereDate");
-            tvDetails.append("\n出生日期：" +Utils.UtcToLocal(PremiereDate)+"\n");
-            tvDetails.append("出生地：" + ProductionLocations == null ? "" : ProductionLocations.toString());
+            tvDetails.append("\n出生日期：" + Utils.UtcToLocal(PremiereDate)+"\n");
+            tvDetails.append("出生地：" + (ProductionLocations == null ? "" : ProductionLocations.toString()));
             fillItemsByPerson(Id);
         }
 
