aeac3008b49d987631c9cdae4aaa6d5538f076f5
sifacaii <sifacai@outlook.com>
2023-05-16 14:12:18 +0800

    添加投屏

---
+++ app/src/main/java/org/sifacai/vlcjellyfin/Dlna/Utils.java
@@ -0,0 +1,37 @@
+package org.sifacai.vlcjellyfin.Dlna;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+
+public class Utils {
+
+    /**
+     * 获取本机IPV4地址
+     * @return
+     */
+    public static String getIpAddress4() {
+        try {
+            Enumeration<NetworkInterface> allNetInterfaces = NetworkInterface.getNetworkInterfaces();
+            InetAddress ip = null;
+            while (allNetInterfaces.hasMoreElements()) {
+                NetworkInterface netInterface = (NetworkInterface) allNetInterfaces.nextElement();
+                if (netInterface.isLoopback() || netInterface.isVirtual() || !netInterface.isUp()) {
+                    continue;
+                } else {
+                    Enumeration<InetAddress> addresses = netInterface.getInetAddresses();
+                    while (addresses.hasMoreElements()) {
+                        ip = addresses.nextElement();
+                        if (ip != null && ip instanceof Inet4Address) {
+                            return ip.getHostAddress();
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("IP地址获取失败" + e.toString());
+        }
+        return "";
+    }
+}
